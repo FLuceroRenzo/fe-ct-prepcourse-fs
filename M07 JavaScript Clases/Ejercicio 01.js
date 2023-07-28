633a12eb89544971522fd40b2d935f9eab8b22e7
FLuceroRenzo <137410087+FLuceroRenzo@users.noreply.github.com>
2023-07-28 06:11:08 -0300

    Add files via upload

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -10,19 +10,54 @@ function crearUsuario() {
    // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
    // Retornar la clase.
    // Tu código:
+   class Usuario {
+      constructor(usuario, nombre, email, password) {
+         this.usuario = usuario;
+         this.nombre = nombre;
+         this.email = email;
+         this.password = password;
+      }
+      saludar () {
+         return ('Hola, mi nombre es ' + this.nombre)
+      }
+   }
+   return Usuario;
 }
 
 function agregarMetodoPrototype(Usuario) {
    // Agrega un método al prototipo de "Usuario".
    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
    // Tu código:
+   Usuario.prototype.saludar = function () {
+      return('Hello World!');
+   }
 }
+
+//class LatinoAmerica {
+//	constructor() {
+//		this.paises = [ ];
+//	};
+//};
+
+//LatinoAmerica.prototype.agregarPais = function (pais) {
+//	this.paises.push(pais);
+//};
+//let continente = new LatinoAmerica();
+//continente.agregarPais('México');
+//console.log(continente.paises);
 
 function agregarStringInvertida() {
    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
    // [PISTA]: necesitarás utilizar el objeto "this".
-}
+   String.prototype.reverse = function () {
+      let palabraInvertida = '';
+      for (i = this.length - 1; i >= 0; i--) {
+         palabraInvertida = palabraInvertida + this[i];
+      }
+      return palabraInvertida;
+   }
+} 
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
