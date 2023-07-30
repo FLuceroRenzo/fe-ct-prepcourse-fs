8fbb82d951c26700f243a6b0b0e043f3d30b18cc
FLuceroRenzo <137410087+FLuceroRenzo@users.noreply.github.com>
2023-07-29 23:41:44 -0300

    Add files via upload

---
+++ Ejercicios Extra.js
@@ -0,0 +1,115 @@
+/*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
+
+function deObjetoAarray(objeto) {
+   // Recibes un objeto. Tendrás que crear un arreglo de arreglos.
+   // Cada elemento del arreglo padre será un nuevo arreglo que contendrá dos elementos.
+   // Estos elementos debe ser cada par clave:valor del objeto recibido.
+   // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
+   // Tu código:
+   let nuevoArreglo = []
+   nuevoArreglo = Object.entries(objeto);
+   return nuevoArreglo;
+}
+
+function numberOfCharacters(string) {
+   // La función recibe un string. Debes recorrerlo y retornar un objeto donde cada propiedad es una de las
+   // letras del string, y su valor es la cantidad de veces que se repite en el string.
+   // Las letras deben estar en orden alfabético.
+   // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
+   // Tu código:
+   let repeticiones = {};
+   for (let i = 0; i < string.length; i++) {
+      let letra = string[i];
+      if (repeticiones[letra]) {
+         repeticiones[letra]++;
+      } else {
+         repeticiones[letra] = 1;
+      }
+   }
+   let letrasOrdenadas = Object.keys(repeticiones).sort();
+   let resultado = {};
+   letrasOrdenadas.forEach(letra => {
+      resultado[letra] = repeticiones[letra];
+   });
+   return resultado;
+}
+
+function capToFront(string) {
+   // Recibes un string con algunas letras en mayúscula y otras en minúscula.
+   // Debes enviar todas las letras en mayúscula al comienzo del string.
+   // Retornar el string.
+   // [EJEMPLO]: soyHENRY ---> HENRYsoy
+   // Tu código:
+   let mayusculas = string.match(/[A-Z]/g) || [];
+   let minusculas = string.match(/[a-z]/g) || [];
+   let resultado = mayusculas.join('') + minusculas.join('');
+   return resultado;
+}
+
+function asAmirror(frase) {
+   // Recibes una frase. Tu tarea es retornar un nuevo string en el que el orden de las palabras sea el mismo.
+   // La diferencia es que cada palabra estará escrita al inverso.
+   // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
+   // Tu código:
+   let palabras = frase.split(' ');
+   let palabrasInvertidas = palabras.map(palabra => {
+      return palabra.split('').reverse().join('');
+   });
+   let resultado = palabrasInvertidas.join(' ');
+   return resultado;
+}
+
+function capicua(numero) {
+   // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
+   // Caso contrario: "No es capicua".
+   // Tu código:
+   let numeroComoTexto = numero.toString();
+   let numeroInvertido = numeroComoTexto.split('').reverse().join('');
+   if (numeroComoTexto === numeroInvertido) {
+      return "Es capicua";
+   } else {
+      return "No es capicua";
+   }
+}
+
+function deleteAbc(string) {
+   // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
+   // Retorna el string sin estas letras.
+   // Tu código:
+   let nuevoTexto = string.replace(/[abc]/g, '');
+   return nuevoTexto;
+}
+
+function sortArray(arrayOfStrings) {
+   // Recibes un arreglo de strings.
+   // Debe retornar un nuevo arreglo, pero con las palabras ordenadas en orden creciente a partir
+   // de la longitud de cada string.
+   // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
+   // Tu código:
+   const nuevoArreglo = arrayOfStrings.sort((a, b) => a.length - b.length);
+   return nuevoArreglo;
+}
+
+function buscoInterseccion(array1, array2) {
+   // Recibes dos arreglos de números.
+   // Debes retornar un nuevo arreglo en el que se guarden los elementos en común entre ambos arreglos.
+   // [EJEMPLO]: [4,2,3] U [1,3,4] = [3,4].
+   // Si no tienen elementos en común, retornar un arreglo vacío.
+   // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
+   // Tu código:
+   let elementosComunes = array1.filter(elemento => array2.includes(elemento));
+   return elementosComunes;
+}
+
+
+/*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
+module.exports = {
+   deObjetoAarray,
+   numberOfCharacters,
+   capToFront,
+   asAmirror,
+   capicua,
+   deleteAbc,
+   sortArray,
+   buscoInterseccion,
+};
